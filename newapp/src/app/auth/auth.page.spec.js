56f71590a4f0acd234f16087b316834e6feedc1c
nekonina <iri_marcanito@hotmail.com>
2019-08-27 18:32:43 -0400

    listo hasta el 117

---
+++ newapp/src/app/auth/auth.page.spec.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var testing_1 = require("@angular/core/testing");
+var auth_page_1 = require("./auth.page");
+describe('AuthPage', function () {
+    var component;
+    var fixture;
+    beforeEach(testing_1.async(function () {
+        testing_1.TestBed.configureTestingModule({
+            declarations: [auth_page_1.AuthPage],
+            schemas: [core_1.CUSTOM_ELEMENTS_SCHEMA],
+        })
+            .compileComponents();
+    }));
+    beforeEach(function () {
+        fixture = testing_1.TestBed.createComponent(auth_page_1.AuthPage);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+    it('should create', function () {
+        expect(component).toBeTruthy();
+    });
+});
+//# sourceMappingURL=auth.page.spec.js.map
